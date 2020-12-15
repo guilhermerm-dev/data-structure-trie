df9512d7d8bc89dfe90b8fad5c95e61afc71e455
Guilherme Ramos Mendes <guilhermerm.ciandt@sulamerica.com.br>
2020-12-15 08:51:39 -0300

    Trie implementation

---
+++ data-structure-trie/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace data_structure_trie
+{
+    public class Node
+    {
+        public Node()
+        {
+            Children = new Dictionary<char, Node>();
+        }
+
+        public Dictionary<char, Node> Children
+        {
+            get;
+            private set;
+        }
+
+        public bool IsCompleteWord
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ data-structure-trie/Trie.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace data_structure_trie
+{
+    public struct Trie
+    {
+        private readonly Node Root;
+
+        public void Insert(string word)
+        {
+            Node current = Root;
+            char[] characteres = word.ToCharArray();
+            foreach (char c in characteres)
+            {
+                Node child = current.Children.GetValueOrDefault(c);
+                if (child is null)
+                    child.Children.Add(c, new Node());
+                current = child;
+            }
+            current.IsCompleteWord = true;
+        }
+
+        public bool Search(string word)
+        {
+            Node node = GetNode(word);
+            return GetNode(word) is not null && node.IsCompleteWord;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            return GetNode(prefix) is not null;
+        }
+
+        private Node GetNode(string word)
+        {
+            Node current = Root;
+            char[] characteres = word.ToCharArray();
+            foreach (var c in characteres)
+            {
+                Node child = current.Children.GetValueOrDefault(c);
+                if (child is null) return null;
+                current = child;
+            }
+            return current;
+        }
+
+        //public int GetWordsCountStartsWithPrefix(string prefix)
+        //{
+
+        //}
+    }
+}
